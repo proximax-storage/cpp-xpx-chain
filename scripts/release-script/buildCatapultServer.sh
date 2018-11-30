0dcafd7c4e68a96394d2e44deaa9092fea21c827
Green <XgreenX9999@gmail.com>
2018-11-30 17:43:10 +0200

    Moved tools to catapulte image

--- scripts/release-script/buildCatapultServer.sh
+++ scripts/release-script/buildCatapultServer.sh
@@ -7,38 +7,50 @@ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-pthread" ..
 make publish
 # First we build extensions which catapult.server required during runtime
 make \
-	catapult.mongo.plugins.aggregate \
-	catapult.mongo.plugins.lock \
-	catapult.mongo.plugins.multisig \
-	catapult.mongo.plugins.namespace \
-	catapult.mongo.plugins.transfer \
-	catapult.plugins.aggregate \
-	catapult.plugins.hashcache \
-	catapult.plugins.lock \
-	catapult.plugins.multisig \
-	catapult.plugins.namespace \
-	catapult.plugins.signature \
-	catapult.plugins.transfer \
-	extension.addressextraction \
-	extension.diagnostics \
-	extension.eventsource \
-	extension.filechain \
-	extension.harvesting \
-	extension.hashcache \
-	extension.mongo \
-	extension.networkheight \
-	extension.nodediscovery \
-	extension.packetserver \
-	extension.partialtransaction \
-	extension.sync \
-	extension.syncsource \
-	extension.timesync \
-	extension.transactionsink \
-	extension.unbondedpruning \
-	extension.zeromq \
-	-j4
-# Then we build catapult.server
-make catapult.server -j4
+    # Required extensions
+    catapult.mongo.plugins.aggregate \
+    catapult.mongo.plugins.lockhash \
+    catapult.mongo.plugins.locksecret \
+    catapult.mongo.plugins.multisig \
+    catapult.mongo.plugins.namespace \
+    catapult.mongo.plugins.property \
+    catapult.mongo.plugins.reputation \
+    catapult.mongo.plugins.transfer \
+    catapult.plugins.aggregate \
+    catapult.plugins.hashcache \
+    catapult.plugins.lockhash \
+    catapult.plugins.locksecret \
+    catapult.plugins.multisig \
+    catapult.plugins.namespace \
+    catapult.plugins.property \
+    catapult.plugins.reputation \
+    catapult.plugins.signature \
+    catapult.plugins.transfer \
+    extension.addressextraction \
+    extension.diagnostics \
+    extension.eventsource \
+    extension.filechain \
+    extension.harvesting \
+    extension.hashcache \
+    extension.mongo \
+    extension.networkheight \
+    extension.nodediscovery \
+    extension.packetserver \
+    extension.partialtransaction \
+    extension.sync \
+    extension.syncsource \
+    extension.timesync \
+    extension.transactionsink \
+    extension.unbondedpruning \
+    extension.zeromq \
+    # Tools
+    catapult.tools.address \
+    catapult.tools.health \
+    catapult.tools.nemgen \
+    catapult.tools.network \
+    # Catapult
+    catapult.server \
+    -j4
 cd ..
 
 # Now we want to create a docker image,
