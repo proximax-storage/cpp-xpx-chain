91e28cd4a15d691490fa93ec397aaa56a72ae060
Oleg Tsybizov <otsybizov@gmail.com>
2019-04-18 12:05:23 +0300

    Replaced tabs with spaces in MetadataTransactionPlugin.cpp

--- plugins/txes/metadata/src/plugins/MetadataTransactionPlugin.cpp
+++ plugins/txes/metadata/src/plugins/MetadataTransactionPlugin.cpp
@@ -18,63 +18,63 @@ using namespace catapult::model;
 
 namespace catapult { namespace plugins {
 
-	namespace {
-		struct AddressTraits {
-			using ModifyMetadataNotification = ModifyAddressMetadataNotification;
-			using ModifyMetadataValueNotification = ModifyAddressMetadataValueNotification;
-		};
+    namespace {
+        struct AddressTraits {
+            using ModifyMetadataNotification = ModifyAddressMetadataNotification;
+            using ModifyMetadataValueNotification = ModifyAddressMetadataValueNotification;
+        };
 
-		struct MosaicTraits {
-			using ModifyMetadataNotification = ModifyMosaicMetadataNotification ;
-			using ModifyMetadataValueNotification = ModifyMosaicMetadataValueNotification;
-		};
+        struct MosaicTraits {
+            using ModifyMetadataNotification = ModifyMosaicMetadataNotification ;
+            using ModifyMetadataValueNotification = ModifyMosaicMetadataValueNotification;
+        };
 
-		struct NamespaceTraits {
-			using ModifyMetadataNotification = ModifyNamespaceMetadataNotification ;
-			using ModifyMetadataValueNotification = ModifyNamespaceMetadataValueNotification;
-		};
+        struct NamespaceTraits {
+            using ModifyMetadataNotification = ModifyNamespaceMetadataNotification ;
+            using ModifyMetadataValueNotification = ModifyNamespaceMetadataValueNotification;
+        };
 
-		template<typename TTraits>
-		class Publisher {
-		public:
-			template<typename TTransaction>
-			static void Publish(const TTransaction& transaction, NotificationSubscriber& sub) {
-				sub.notify(MetadataTypeNotification(transaction.MetadataType));
-				sub.notify(CreateMetadataModificationsNotification<TTransaction>(transaction));
+        template<typename TTraits>
+        class Publisher {
+        public:
+            template<typename TTransaction>
+            static void Publish(const TTransaction& transaction, NotificationSubscriber& sub) {
+                sub.notify(MetadataTypeNotification(transaction.MetadataType));
+                sub.notify(CreateMetadataModificationsNotification<TTransaction>(transaction));
 
-				std::vector<const model::MetadataModification*> modifications;
+                std::vector<const model::MetadataModification*> modifications;
 
-				for (const auto& modification : transaction.Transactions())
-					modifications.emplace_back(&modification);
+                for (const auto& modification : transaction.Transactions())
+                    modifications.emplace_back(&modification);
 
-				if (!modifications.empty())
+                if (!modifications.empty())
                     sub.notify(MetadataModificationsNotification(
-                            state::GetHash(state::ToVector(transaction.MetadataId), transaction.MetadataType),
-                            modifications));
+                        state::GetHash(state::ToVector(transaction.MetadataId), transaction.MetadataType),
+                        modifications));
 
-				for (const auto& modification : transaction.Transactions()) {
-					sub.notify(ModifyMetadataFieldNotification(
-							modification.ModificationType,
-							modification.KeySize, modification.KeyPtr(),
-							modification.ValueSize, modification.ValuePtr()));
+                for (const auto& modification : transaction.Transactions()) {
+                    sub.notify(ModifyMetadataFieldNotification(
+                        modification.ModificationType,
+                        modification.KeySize, modification.KeyPtr(),
+                        modification.ValueSize, modification.ValuePtr()));
 
-					sub.notify(typename TTraits::ModifyMetadataValueNotification(
-							transaction.MetadataId, transaction.MetadataType,
-							modification.ModificationType,
-							modification.KeySize, modification.KeyPtr(),
-							modification.ValueSize, modification.ValuePtr()));
-				}
-			}
+                    sub.notify(typename TTraits::ModifyMetadataValueNotification(
+                        transaction.MetadataId, transaction.MetadataType,
+                        modification.ModificationType,
+                        modification.KeySize, modification.KeyPtr(),
+                        modification.ValueSize, modification.ValuePtr()));
+                }
+            }
 
-		private:
-			template<typename TTransaction>
-			static auto CreateMetadataModificationsNotification(const TTransaction& transaction) {
-				return typename TTraits::ModifyMetadataNotification(transaction.Signer, transaction.MetadataId);
-			}
-		};
-	}
+        private:
+            template<typename TTransaction>
+            static auto CreateMetadataModificationsNotification(const TTransaction& transaction) {
+                return typename TTraits::ModifyMetadataNotification(transaction.Signer, transaction.MetadataId);
+            }
+        };
+    }
 
-	DEFINE_TRANSACTION_PLUGIN_FACTORY(AddressMetadata, Publisher<AddressTraits>::Publish)
-	DEFINE_TRANSACTION_PLUGIN_FACTORY(MosaicMetadata, Publisher<MosaicTraits>::Publish)
-	DEFINE_TRANSACTION_PLUGIN_FACTORY(NamespaceMetadata, Publisher<NamespaceTraits>::Publish)
+    DEFINE_TRANSACTION_PLUGIN_FACTORY(AddressMetadata, Publisher<AddressTraits>::Publish)
+    DEFINE_TRANSACTION_PLUGIN_FACTORY(MosaicMetadata, Publisher<MosaicTraits>::Publish)
+    DEFINE_TRANSACTION_PLUGIN_FACTORY(NamespaceMetadata, Publisher<NamespaceTraits>::Publish)
 }}
