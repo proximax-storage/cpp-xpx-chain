5f284ddd3411d43fe0ae733520ebd2f69a72b981
Oleg Tsybizov <otsybizov@gmail.com>
2019-10-28 15:35:17 +0200

    [PC-204] Fixes according to review comments.

--- plugins/txes/exchange/src/observers/OfferObserver.cpp
+++ plugins/txes/exchange/src/observers/OfferObserver.cpp
@@ -11,10 +11,11 @@ namespace catapult { namespace observers {
 	DEFINE_OBSERVER(Offer, model::OfferNotification<1>, [](const auto& notification, const ObserverContext& context) {
 		auto& cache = context.Cache.sub<cache::OfferCache>();
 		if (NotifyMode::Commit == context.Mode) {
-			auto deadline = Height(-1);
-			if (notification.Duration.unwrap() < BlockDuration(-1).unwrap() - context.Height.unwrap())
-				deadline = Height(context.Height.unwrap() + notification.Duration.unwrap());
-			state::OfferEntry entry(notification.TransactionHash, notification.Signer, notification.OfferType, deadline);
+			auto maxDeadline = std::numeric_limits<Height::ValueType>::max();
+			auto duration = notification.Duration.unwrap();
+			auto currentHeight = context.Height.unwrap();
+			auto deadline = (duration < maxDeadline - currentHeight) ? currentHeight + duration : maxDeadline;
+			state::OfferEntry entry(notification.TransactionHash, notification.Signer, notification.OfferType, Height(deadline));
 			auto pOffer = notification.OffersPtr;
 			for (uint8_t i = 0; i < notification.OfferCount; ++i, ++pOffer) {
 				entry.initialOffers().emplace(pOffer->Mosaic.MosaicId, *pOffer);
