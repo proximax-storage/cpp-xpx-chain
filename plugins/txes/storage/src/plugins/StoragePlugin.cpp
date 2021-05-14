d5bba8d0e8b7c961d386e4b5a540b78495dce4dc
oleg <otsybizov@gmail.com>
2021-05-14 14:26:00 +0300

    Fixed build errors.

--- plugins/txes/storage/src/plugins/StoragePlugin.cpp
+++ plugins/txes/storage/src/plugins/StoragePlugin.cpp
@@ -69,6 +69,7 @@ namespace catapult { namespace plugins {
 				.add(observers::CreatePrepareDriveObserver())
 				.add(observers::CreateDownloadChannelObserver())
 				.add(observers::CreateDataModificationApprovalObserver());
+		});
 	}
 }}
 
