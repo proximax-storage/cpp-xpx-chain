b55d42ce87dbb1e9279e0afa11a7e14f03da85ba
wernling-proximax <wernling.tan@proximax.io>
2021-06-30 04:23:20 +0800

    Added BcDrive checks in StorageTestUtils

---
+++ plugins/txes/storage/tests/test/StorageTestUtils.cpp
@@ -0,0 +1,138 @@
+/**
+*** Copyright 2019 ProximaX Limited. All rights reserved.
+*** Use of this source code is governed by the Apache 2.0
+*** license that can be found in the LICENSE file.
+**/
+
+#include "StorageTestUtils.h"
+#include "tests/TestHarness.h"
+
+namespace catapult { namespace test {
+
+    namespace {
+        void AssertEqualActiveDataModifications(const std::vector<state::ActiveDataModification>& expectedActiveDataModifications, const std::vector<state::ActiveDataModification>& activeDataModifications) {
+            ASSERT_EQ(expectedActiveDataModifications.size(), activeDataModifications.size());
+            for (auto i = 0u; i < activeDataModifications.size(); i++) {
+                const auto &expectedActiveDataModification = expectedActiveDataModifications[i];
+                const auto &activeDataModification = activeDataModifications[i];
+                EXPECT_EQ(expectedActiveDataModification.Id, activeDataModification.Id);
+                EXPECT_EQ(expectedActiveDataModification.Owner, activeDataModification.Owner);
+                EXPECT_EQ(expectedActiveDataModification.DownloadDataCdi, activeDataModification.DownloadDataCdi);
+                EXPECT_EQ(expectedActiveDataModification.UploadSize, activeDataModification.UploadSize);
+            }
+        }
+
+        void AssertEqualCompletedDataModifications(const std::vector<state::CompletedDataModification>& expectedCompletedDataModifications, const std::vector<state::CompletedDataModification>& completedDataModifications) {
+            ASSERT_EQ(expectedCompletedDataModifications.size(), completedDataModifications.size());
+            for (auto i = 0u; i < completedDataModifications.size(); i++) {
+                const auto &expectedCompletedDataModification = expectedCompletedDataModifications[i];
+                const auto &completedDataModification = completedDataModifications[i];
+                EXPECT_EQ(expectedCompletedDataModifications[i].Id,completedDataModifications[i].Id);
+                 EXPECT_EQ(expectedCompletedDataModifications[i].Owner,completedDataModifications[i].Owner);
+                EXPECT_EQ(expectedCompletedDataModifications[i].DownloadDataCdi,completedDataModifications[i].DownloadDataCdi);
+                EXPECT_EQ(expectedCompletedDataModifications[i].UploadSize,completedDataModifications[i].UploadSize);
+                EXPECT_EQ(expectedCompletedDataModifications[i].State,completedDataModifications[i].State);
+            }
+        }
+    }
+
+    state::BcDriveEntry CreateBcDriveEntry(
+            Key key,
+            Key owner,
+            Hash256 rootHash,
+	    	uint64_t size,
+	    	uint16_t replicatorCount,
+	    	uint16_t activeDataModificationsCount,
+		    uint16_t completedDataModificationsCount,
+		    uint16_t activeDownloadsCount,
+		    uint16_t completedDownloadsCount) {
+        state::BcDriveEntry entry(key);
+        entry.setOwner(owner);
+        entry.setRootHash(rootHash);
+        entry.setSize(size);
+        entry.setReplicatorCount(replicatorCount);
+
+        entry.activeDataModifications().reserve(activeDataModificationsCount);
+        for (auto aDMC = 0u; aDMC < activeDataModificationsCount; ++aDMC){
+            entry.activeDataModifications().emplace_back(state::ActiveDataModification{
+                test::GenerateRandomByteArray<Hash256>(),   /// Id of data modification.
+                key,                                        /// Public key of the drive owner.
+                test::GenerateRandomByteArray<Hash256>(),   /// CDI of download data.
+                test::Random()                              /// Upload size of data.
+            });
+        }
+
+        entry.completedDataModifications().reserve(completedDataModificationsCount);
+        for (auto cDMC = 0u; cDMC < completedDataModificationsCount; ++cDMC){
+            entry.completedDataModifications().emplace_back(state::CompletedDataModification{
+                state::CompletedDataModification(entry.activeDataModifications()[cDMC], static_cast<state::DataModificationState>(test::RandomByte())),
+                static_cast<state::DataModificationState>(test::RandomByte())
+            });
+        }
+
+        for (auto aDC = 0u; aDC < activeDownloadsCount; ++aDC) {
+            entry.activeDownloads().emplace_back(test::GenerateRandomByteArray<Hash256>());
+        }
+
+        for (auto cDC = 0u; cDC < completedDownloadsCount; ++cDC) {
+            entry.completedDownloads().emplace_back(test::GenerateRandomByteArray<Hash256>());
+        }
+
+        return entry;
+    }
+
+    void AssertEqualBcDriveData(const state::BcDriveEntry& expectedEntry, const state::BcDriveEntry& entry) {
+        EXPECT_EQ(expectedEntry.key(), entry.key());
+        EXPECT_EQ(expectedEntry.owner(), entry.owner());
+        EXPECT_EQ(expectedEntry.rootHash(), entry.rootHash());
+        EXPECT_EQ(expectedEntry.size(), entry.size());
+        EXPECT_EQ(expectedEntry.replicatorCount(), entry.replicatorCount());
+
+        AssertEqualActiveDataModifications(expectedEntry.activeDataModifications(), entry.activeDataModifications());
+        AssertEqualCompletedDataModifications(expectedEntry.completedDataModifications(), entry.completedDataModifications());
+
+        const auto& expectedActiveDownloads = expectedEntry.activeDownloads();
+		const auto& activeDownloads = entry.activeDownloads();
+        ASSERT_EQ(expectedActiveDownloads.size(), activeDownloads.size());
+        for (auto i = 0u; i < activeDownloads.size(); ++i) {
+            const auto& expectedActiveDownload = expectedActiveDownloads[i];
+			const auto& activeDownload = activeDownloads[i];
+            EXPECT_EQ(expectedActiveDownload.size(), activeDownload.size());
+        }
+
+        const auto& expectedCompletedDownloads = expectedEntry.completedDownloads();
+		const auto& completedDownloads = entry.completedDownloads();
+        ASSERT_EQ(expectedCompletedDownloads.size(), completedDownloads.size());
+        for (auto i = 0u; i < completedDownloads.size(); ++i) {
+            const auto& expectedCompletedDownload = expectedCompletedDownloads[i];
+			const auto& completedDownload = completedDownloads[i];
+            EXPECT_EQ(expectedCompletedDownload.size(), completedDownload.size());
+        }
+    }
+
+    state::DownloadChannelEntry CreateDownloadChannelEntry(
+            Hash256 id,
+            Key consumer,
+		    Key drive,
+    		Amount transactionFee,
+	    	Amount storageUnits) {
+        state::DownloadChannelEntry entry(id);
+        entry.setConsumer(consumer);
+        entry.setDrive(drive);
+        entry.setTransactionFee(transactionFee);
+        entry.setStorageUnits(storageUnits);
+
+        return entry;
+    }
+
+    void AssertEqualDownloadChannelData(const state::DownloadChannelEntry& expectedEntry, const state::DownloadChannelEntry& entry) {
+        EXPECT_EQ(expectedEntry.id(), entry.id());
+        EXPECT_EQ(expectedEntry.consumer(), entry.consumer());
+        EXPECT_EQ(expectedEntry.drive(), entry.drive());
+        EXPECT_EQ(expectedEntry.transactionFee(), entry.transactionFee());
+        EXPECT_EQ(expectedEntry.storageUnits(), entry.storageUnits());
+    }
+
+}}
+
+
---
+++ plugins/txes/storage/tests/test/StorageTestUtils.h
@@ -0,0 +1,75 @@
+/**
+*** Copyright 2019 ProximaX Limited. All rights reserved.
+*** Use of this source code is governed by the Apache 2.0
+*** license that can be found in the LICENSE file.
+**/
+
+#pragma once
+#include "src/cache/BcDriveCache.h"
+#include "src/cache/BcDriveCacheStorage.h"
+#include "src/cache/DownloadChannelCache.h"
+#include "src/cache/DownloadChannelCacheStorage.h"
+#include "src/cache/ReplicatorCache.h"
+#include "src/cache/ReplicatorCacheStorage.h"
+#include "src/model/StorageEntityType.h"
+#include "tests/test/cache/CacheTestUtils.h"
+#include "tests/test/core/mocks/MockBlockchainConfigurationHolder.h"
+#include "tests/test/other/MutableBlockchainConfiguration.h"
+#include "tests/test/nodeps/Random.h"
+
+namespace catapult { namespace test {
+
+    /// Creates test drive entry.
+    state::BcDriveEntry CreateBcDriveEntry(
+        Key key = test::GenerateRandomByteArray<Key>(),
+        Key owner = test::GenerateRandomByteArray<Key>(),
+        Hash256 rootHash = test::GenerateRandomByteArray<Hash256>(),
+		uint64_t size = test::Random(),
+		uint16_t replicatorCount = test::Random16(),
+		uint16_t activeDataModificationsCount = 2,
+		uint16_t completedDataModificationsCount = 2,
+		uint16_t activeDownloadsCount = 2,
+		uint16_t completedDownloadsCount = 2
+    );
+
+	/// Verifies that \a entry1 is equivalent to \a entry2.
+    void AssertEqualBcDriveData(const state::BcDriveEntry& expectedEntry, const state::BcDriveEntry& entry);
+
+    /// Cache factory for creating a catapult cache composed of bc drive cache and core caches.
+    struct BcDriveCacheFactory {
+        private:
+            static auto CreateSubCacheWithBcDriveCache(const config::BlockchainConfiguration& config) {
+                auto id = std::max(cache::BcDriveCache::Id, std::max(cache::DownloadChannelCache::Id, cache::ReplicatorCache::Id));
+                std::vector<std::unique_ptr<cache::SubCachePlugin>> subCaches(id + 1);
+			    auto pConfigHolder = config::CreateMockConfigurationHolder(config);
+			    subCaches[cache::BcDriveCache::Id] = MakeSubCachePlugin<cache::BcDriveCache, cache::BcDriveCacheStorage>(pConfigHolder);
+			    subCaches[cache::DownloadChannelCache::Id] = MakeSubCachePlugin<cache::DownloadChannelCache, cache::DownloadChannelCacheStorage>(pConfigHolder);
+                subCaches[cache::ReplicatorCache::Id] = MakeSubCachePlugin<cache::ReplicatorCache, cache::ReplicatorCacheStorage>(pConfigHolder);
+			    return subCaches;
+            }
+
+        public:
+            /// Creates an empty catapult cache around default configuration.
+            static cache::CatapultCache Create() {
+                return Create(test::MutableBlockchainConfiguration().ToConst());
+            }
+
+            /// Creates an empty catapult cache around \a config.
+		    static cache::CatapultCache Create(const config::BlockchainConfiguration& config) {
+                auto subCaches = CreateSubCacheWithBcDriveCache(config);
+                CoreSystemCacheFactory::CreateSubCaches(config, subCaches);
+                return cache::CatapultCache(std::move(subCaches));
+		    }
+    };
+
+    state::DownloadChannelEntry CreateDownloadChannelEntry(
+        Hash256 id = test::GenerateRandomByteArray<Hash256>(),
+        Key consumer = test::GenerateRandomByteArray<Key>(),
+        Key drive = test::GenerateRandomByteArray<Key>(),
+        Amount transactionFee = test::GenerateRandomByteArray<Amount>(),
+        Amount storageUnits  = test::GenerateRandomByteArray<Amount>()
+    );
+
+    void AssertEqualDownloadChannelData(const state::DownloadChannelEntry& expectedEntry, const state::DownloadChannelEntry& entry);
+
+}}
