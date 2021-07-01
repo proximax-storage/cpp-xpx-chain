986e5d426f697d55550ba44f7497a3cafe5392b9
wernling-proximax <wernling.tan@proximax.io>
2021-07-01 12:06:39 +0800

    Added DataModificationApprovalTransactionPluginTests

---
+++ plugins/txes/storage/tests/plugins/DataModificationApprovalTransactionPluginTests.cpp
@@ -0,0 +1,120 @@
+/**
+*** Copyright 2019 ProximaX Limited. All rights reserved.
+*** Use of this source code is governed by the Apache 2.0
+*** license that can be found in the LICENSE file.
+**/
+
+#include "src/plugins/DataModificationApprovalTransactionPlugin.h"
+#include "src/model/DataModificationApprovalTransaction.h"
+#include "src/catapult/model/StorageNotifications.h"
+#include "tests/test/core/mocks/MockNotificationSubscriber.h"
+#include "tests/test/plugins/TransactionPluginTestUtils.h"
+#include "tests/test/StorageTestUtils.h"
+
+using namespace catapult::model;
+
+namespace catapult { namespace plugins {
+
+#define TEST_CLASS DataModificationApprovalTransactionPluginTests
+
+	namespace {
+		DEFINE_TRANSACTION_PLUGIN_TEST_TRAITS(DataModificationApproval, 1, 1,)
+
+		template<typename TTraits>
+		auto CreateTransaction() {
+			return test::CreateDataModificationApprovalTransaction<typename TTraits::TransactionType>();
+		}
+	}
+
+	DEFINE_BASIC_EMBEDDABLE_TRANSACTION_PLUGIN_TESTS(TEST_CLASS,,, Entity_Type_DataModificationApproval)
+
+	PLUGIN_TEST(CanCalculateSize) {
+		// Arrange:
+		auto pPlugin = TTraits::CreatePlugin();
+		auto pTransaction = CreateTransaction<TTraits>();
+
+		// Act:
+		auto realSize = pPlugin->calculateRealSize(*pTransaction);
+
+		// Assert:
+		EXPECT_EQ(sizeof(typename TTraits::TransactionType), realSize);
+	}
+
+	// region publish - basic
+
+	PLUGIN_TEST(PublishesNoNotificationWhenTransactionVersionIsInvalid) {
+		// Arrange:
+		mocks::MockNotificationSubscriber sub;
+		auto pPlugin = TTraits::CreatePlugin();
+
+		typename TTraits::TransactionType transaction;
+		transaction.Version = MakeVersion(NetworkIdentifier::Mijin_Test, std::numeric_limits<uint32_t>::max());
+		transaction.Size = sizeof(transaction);
+
+		// Act:
+		test::PublishTransaction(*pPlugin, transaction, sub);
+
+		// Assert:
+		ASSERT_EQ(0, sub.numNotifications());
+	}
+
+	PLUGIN_TEST(CanPublishCorrectNumberOfNotifications) {
+		// Arrange:
+		auto pTransaction = CreateTransaction<TTraits>();
+		mocks::MockNotificationSubscriber sub;
+		auto pPlugin = TTraits::CreatePlugin();
+
+		// Act:
+		test::PublishTransaction(*pPlugin, *pTransaction, sub);
+
+		// Assert:
+		ASSERT_EQ(1u, sub.numNotifications());
+        auto i = 0u;
+		EXPECT_EQ(DataModificationApprovalNotification, sub.notificationTypes()[i++]);
+	}
+
+	// endregion
+
+	// region publish - drive notification
+
+	PLUGIN_TEST(CanPublishDriveNotification) {
+		// Arrange:
+		mocks::MockTypedNotificationSubscriber<DriveNotification<1>> sub;
+		auto pPlugin = TTraits::CreatePlugin();
+		auto pTransaction = CreateTransaction<TTraits>();
+
+		// Act:
+		test::PublishTransaction(*pPlugin, *pTransaction, sub);
+
+		// Assert:
+		ASSERT_EQ(1u, sub.numMatchingNotifications());
+		const auto& notification = sub.matchingNotifications()[0];
+		EXPECT_EQ(pTransaction->Signer, notification.DriveKey);
+		EXPECT_EQ(Entity_Type_DataModificationApproval, notification.TransactionType);
+	}
+
+	// endregion
+
+	// region publish - data modification approval notification
+
+	PLUGIN_TEST(CanPublishDataModificationApprovalNotification) {
+		// Arrange:
+		mocks::MockTypedNotificationSubscriber<DataModificationApprovalNotification<1>> sub;
+		auto pPlugin = TTraits::CreatePlugin();
+		auto pTransaction = CreateTransaction<TTraits>();
+
+		// Act:
+		test::PublishTransaction(*pPlugin, *pTransaction, sub);
+
+		// Assert:
+		ASSERT_EQ(1u, sub.numMatchingNotifications());
+		const auto& notification = sub.matchingNotifications()[0];
+		EXPECT_EQ(pTransaction->DriveKey, notification.DriveKey);
+        EXPECT_EQ(pTransaction->DataModificationId, notification.DataModificationId);
+        EXPECT_EQ(pTransaction->FileStructureCdi, notification.FileStructureCdi);
+        EXPECT_EQ(pTransaction->FileStructureSize, notification.FileStructureSize);
+        EXPECT_EQ(pTransaction->UsedDriveSize, notification.UsedDriveSize);
+	}
+
+	// endregion
+}}
