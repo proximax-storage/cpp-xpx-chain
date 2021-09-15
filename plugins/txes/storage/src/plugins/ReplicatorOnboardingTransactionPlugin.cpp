d8060cf36bf7be0e9a415b90cd409f0bb5e0ef05
wernling-proximax <wernling.tan@proximax.io>
2021-09-15 13:13:48 +0800

    Updated replicator onboarding unit test

--- plugins/txes/storage/src/plugins/ReplicatorOnboardingTransactionPlugin.cpp
+++ plugins/txes/storage/src/plugins/ReplicatorOnboardingTransactionPlugin.cpp
@@ -21,6 +21,7 @@ namespace catapult { namespace plugins {
 			case 1: {
 				sub.notify(DriveNotification<1>(transaction.Signer, transaction.Type));
 				sub.notify(ReplicatorOnboardingNotification<1>(transaction.Signer, transaction.BlsKey, transaction.Capacity));
+				sub.notify(AccountPublicKeyNotification<1>(transaction.Signer));
 
 //				const auto signerAddress = extensions::CopyToUnresolvedAddress(PublicKeyToAddress(transaction.Signer, config.NetworkIdentifier));
 //				const auto storageMosaicId = config::GetUnresolvedStorageMosaicId(config);
