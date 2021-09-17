15964b09261f4711265a1f6b4f61b7611d14afcb
jackshenn <chanjackshen@gmail.com>
2021-09-17 14:56:25 +0800

    update onboarding observer test

--- plugins/txes/storage/tests/observers/ReplicatorOnboardingObserverTests.cpp
+++ plugins/txes/storage/tests/observers/ReplicatorOnboardingObserverTests.cpp
@@ -20,41 +20,46 @@ namespace catapult { namespace observers {
         using ObserverTestContext = test::ObserverTestContextT<test::ReplicatorCacheFactory>;
         using Notification = model::ReplicatorOnboardingNotification<1>;
 
+        const Key Public_Key = test::GenerateRandomByteArray<Key>();
+        const BLSPublicKey Bls_Key = test::GenerateRandomByteArray<BLSPublicKey>();
+        constexpr auto Capacity = Amount(50);
+        constexpr auto Replicator_Count = 10;
         constexpr auto Current_Height = Height(25);
 
-        struct ReplicatorValues {
+        state::ReplicatorEntry CreateReplicatorEntry() {
+            state::ReplicatorEntry entry(Public_Key);
+            entry.setCapacity(Capacity);
+            entry.setBlsKey(Bls_Key);
+
+            return entry;
+        }
+
+        state::BlsKeysEntry CreateBlsKeyEntry() {
+            state::BlsKeysEntry entry(Bls_Key);
+            entry.setKey(Public_Key);
+
+            return entry;
+        }
+
+        struct CacheValues {
             public:
-                explicit ReplicatorValues()
-                    : PublicKey(test::GenerateRandomByteArray<Key>())
-                    , BlsKey(test::GenerateRandomByteArray<BLSPublicKey>())
-                    , Capacity(test::GenerateRandomValue<Amount>())
+			    explicit CacheValues()
+                    : ReplicatorEntry(Key())
+                    , BlsKeyEntry(BLSPublicKey())
                 {}
 
             public:
-                Key PublicKey;
-                BLSPublicKey BlsKey;
-                Amount Capacity;
+                state::ReplicatorEntry ReplicatorEntry;
+                state::BlsKeysEntry BlsKeyEntry;
         };
 
-        state::ReplicatorEntry CreateReplicatorEntry(const ReplicatorValues& values) {
-            state::ReplicatorEntry entry(values.PublicKey);
-            entry.setCapacity(values.Capacity);
-            entry.setBlsKey(values.BlsKey);
-
-            return entry;
-        }
-
-        state::BlsKeysEntry CreateBlsKeyEntry(const ReplicatorValues& values) {
-            state::BlsKeysEntry entry(values.BlsKey);
-            entry.setKey(values.PublicKey);
-
-            return entry;
-        }
-
-        void RunTest(NotifyMode mode, const ReplicatorValues& values, const Height& currentHeight) {
+        void RunTest(NotifyMode mode, const CacheValues& values, const Height& currentHeight) {
             // Arrange:
             ObserverTestContext context(mode, currentHeight);
-            Notification notification(values.PublicKey, values.BlsKey, values.Capacity);
+            Notification notification(
+                values.ReplicatorEntry.key(),
+                values.ReplicatorEntry.blsKey(),
+                values.ReplicatorEntry.capacity());
             auto pObserver = CreateReplicatorOnboardingObserver();
         	auto& replicatorCache = context.cache().sub<cache::ReplicatorCache>();
             auto& blsKeysCache = context.cache().sub<cache::BlsKeysCache>();
@@ -63,20 +68,22 @@ namespace catapult { namespace observers {
             test::ObserveNotification(*pObserver, notification, context);
 
             // Assert: check the cache
-     		auto replicatorIter = replicatorCache.find(values.PublicKey);
+     		auto replicatorIter = replicatorCache.find(values.ReplicatorEntry.key());
 			const auto &replicatorEntry = replicatorIter.get();
-			test::AssertEqualReplicatorData(CreateReplicatorEntry(values), replicatorEntry);
+			test::AssertEqualReplicatorData(values.ReplicatorEntry, replicatorEntry);
 
-     		auto BlsKeyIter = blsKeysCache.find(values.BlsKey);
+     		auto BlsKeyIter = blsKeysCache.find(values.BlsKeyEntry.blsKey());
 			const auto &blsKeysEntry = BlsKeyIter.get();
-			test::AssertEqualBlskeyData(CreateBlsKeyEntry(values), blsKeysEntry);
+			test::AssertEqualBlskeyData(values.BlsKeyEntry, blsKeysEntry);
 
         }
     }
 
     TEST(TEST_CLASS, ReplicatorOnboarding_Commit) {
         // Arrange:
-        ReplicatorValues values;
+        CacheValues values;
+        values.ReplicatorEntry = CreateReplicatorEntry();
+        values.BlsKeyEntry = CreateBlsKeyEntry();
 
         // Assert:
         RunTest(NotifyMode::Commit, values, Current_Height);
@@ -84,7 +91,9 @@ namespace catapult { namespace observers {
 
     TEST(TEST_CLASS, ReplicatorOnboarding_Rollback) {
         // Arrange:
-        ReplicatorValues values;
+        CacheValues values;
+        values.ReplicatorEntry = CreateReplicatorEntry();
+        values.BlsKeyEntry = CreateBlsKeyEntry();
 
         // Assert
 		EXPECT_THROW(RunTest(NotifyMode::Rollback, values, Current_Height), catapult_runtime_error);
