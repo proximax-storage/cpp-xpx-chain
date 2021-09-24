133a7049b7e537f2b09bb6ac3d7455961237b57c
oleg <otsybizov@gmail.com>
2021-09-24 13:09:08 +0300

    Fixed replicator offboarding observer.

--- plugins/txes/storage/tests/observers/ReplicatorOffboardingObserverTests.cpp
+++ plugins/txes/storage/tests/observers/ReplicatorOffboardingObserverTests.cpp
@@ -5,7 +5,6 @@
 **/
 
 #include "tests/test/StorageTestUtils.h"
-#include "catapult/model/StorageNotifications.h"
 #include "src/observers/Observers.h"
 #include "tests/test/plugins/ObserverTestUtils.h"
 #include "tests/TestHarness.h"
@@ -22,97 +21,91 @@ namespace catapult { namespace observers {
         using ObserverTestContext = test::ObserverTestContextT<test::ReplicatorCacheFactory>;
         using Notification = model::ReplicatorOffboardingNotification<1>;
 
-        const Key Replicator_Key = test::GenerateRandomByteArray<Key>();
-        const Key Drive_Key = test::GenerateRandomByteArray<Key>();
-        const Key Owner_key = test::GenerateRandomByteArray<Key>();
+		Key Replicator_Key = test::GenerateRandomByteArray<Key>();
+		Key Drive_Key1 = test::GenerateRandomByteArray<Key>();
+		Key Drive_Key2 = test::GenerateRandomByteArray<Key>();
         constexpr auto Current_Height = Height(25);
-        uint64_t Storage_Deposit = 100;
-        uint64_t Streaming_Deposit = 200;
+		constexpr auto Capacity = Amount(30);
+		constexpr uint64_t Drive1_Size = 100;
+		constexpr uint64_t Drive1_Used_Size = 40;
+		constexpr uint64_t Drive2_Size = 200;
+		constexpr uint64_t Drive2_Confirmed_Used_Size = 50;
+		constexpr uint64_t Drive2_Used_Size = 60;
+		constexpr uint64_t Expected_Storage_Deposit_Return = Capacity.unwrap() + Drive1_Size + Drive2_Size;
+		constexpr uint64_t Expected_Streaming_Deposit_Return = 2 * (Expected_Storage_Deposit_Return - Drive1_Used_Size - Drive2_Confirmed_Used_Size);
+		constexpr auto Expected_Replicator_Balance = Amount(Expected_Storage_Deposit_Return + Expected_Streaming_Deposit_Return);
+		constexpr auto Currency_Mosaic_Id = MosaicId(1234);
 
-        state::BcDriveEntry CreateBcDriveEntry() {
-            state::BcDriveEntry entry(Drive_Key);
-            entry.setOwner(Owner_key);
-            entry.setUsedSize(25);
-            entry.confirmedUsedSizes().insert({Replicator_Key, 25});
+		auto CreateConfig() {
+			test::MutableBlockchainConfiguration config;
+			config.Immutable.CurrencyMosaicId = Currency_Mosaic_Id;
+			return config.ToConst();
+		}
 
-            return entry;
+        std::vector<state::BcDriveEntry> CreateDriveEntries() {
+			std::vector<state::BcDriveEntry> ret;
+            state::BcDriveEntry entry1(Drive_Key1);
+            entry1.setSize(Drive1_Size);
+            entry1.setUsedSize(Drive1_Used_Size);
+			ret.push_back(entry1);
+
+            state::BcDriveEntry entry2(Drive_Key2);
+            entry2.setSize(Drive2_Size);
+            entry2.setUsedSize(Drive2_Used_Size);
+            entry2.confirmedUsedSizes().insert({ Replicator_Key, Drive2_Confirmed_Used_Size });
+			ret.push_back(entry2);
+
+            return ret;
         }
 
         state::ReplicatorEntry CreateReplicatorEntry() {
             state::ReplicatorEntry entry(Replicator_Key);
-            entry.setCapacity(Amount(Storage_Deposit));
-            entry.drives().emplace(Drive_Key, state::DriveInfo{});
+            entry.setCapacity(Capacity);
+            entry.drives().emplace(Drive_Key1, state::DriveInfo{});
+            entry.drives().emplace(Drive_Key2, state::DriveInfo{});
             return entry;
         }
 
-        state::AccountState CreateAccount(catapult::MosaicId Storage_Mosaic_Id, catapult::MosaicId Streaming_Mosaic_Id) {
+        state::AccountState CreateAccount() {
 		    auto address = model::PublicKeyToAddress(Replicator_Key, model::NetworkIdentifier::Mijin_Test);
 		    state::AccountState account(address, Current_Height);
 		    account.PublicKey = Replicator_Key;
 		    account.PublicKeyHeight = Current_Height;
-            account.Balances.credit(Storage_Mosaic_Id, Amount(Storage_Deposit));
-            account.Balances.credit(Streaming_Mosaic_Id, Amount(Streaming_Deposit));
 		    return account;
 	    }
 
-        struct ReplicatorValues {
-            public:
-                explicit ReplicatorValues()
-                    : PublicKey(test::GenerateRandomByteArray<Key>())
-                {}
-
-            public:
-                Key PublicKey;
-        };
-
-        static test::BalanceTransfers GetFinalReplicatorStorage(MosaicId mosaicId) {
-            //total storage deposit = 100 (capacity + usedSize)
-            return { { mosaicId, Amount(0) } };
-        }
-        
-        static test::BalanceTransfers GetFinalReplicatorStreaming(MosaicId mosaicId) {
-            //total streaming deposit = 50   (2*50)-(2min(25, 25))   
-            return { { mosaicId, Amount(200 - 50) } };
-        }
-        
-        static test::BalanceTransfers GetFinalReplicatorCurrency(MosaicId mosaicId) {
-            //storage + streaming = 150
-            return { { mosaicId, Amount(100) } };
-        }
-
-        void RunTest(NotifyMode mode, ReplicatorValues values, const Height& Current_Height) {
+        void RunTest(NotifyMode mode) {
             // Arrange:
-            ObserverTestContext context(mode, Current_Height);
+            ObserverTestContext context(mode, Current_Height, CreateConfig());
             Notification notification(Replicator_Key);
             auto pObserver = CreateReplicatorOffboardingObserver();
         	auto& replicatorCache = context.cache().sub<cache::ReplicatorCache>();
             auto& accountCache = context.cache().sub<cache::AccountStateCache>();
             auto& driveCache = context.cache().sub<cache::BcDriveCache>();
-            auto& Currency_Mosaic_Id = context.observerContext().Config.Immutable.CurrencyMosaicId;
-            auto& Storage_Mosaic_Id = context.observerContext().Config.Immutable.CurrencyMosaicId;
-            auto& Streaming_Mosaic_Id = context.observerContext().Config.Immutable.CurrencyMosaicId;
 
             //Populate cache
             replicatorCache.insert(CreateReplicatorEntry());
-            driveCache.insert(CreateBcDriveEntry());
-            accountCache.addAccount(CreateAccount(Storage_Mosaic_Id, Streaming_Mosaic_Id));
+			auto driveEntries = CreateDriveEntries();
+			for (const auto& driveEntry : driveEntries)
+            	driveCache.insert(driveEntry);
+            accountCache.addAccount(CreateAccount());
 
             // Act:
             test::ObserveNotification(*pObserver, notification, context);
 
             // Assert: check the cache
 			EXPECT_FALSE(replicatorCache.find(Replicator_Key).tryGet());
-            test::AssertBalances(context.cache(), values.PublicKey, GetFinalReplicatorCurrency(Currency_Mosaic_Id));
-            test::AssertBalances(context.cache(), values.PublicKey, GetFinalReplicatorStorage(Storage_Mosaic_Id));
-            test::AssertBalances(context.cache(), values.PublicKey, GetFinalReplicatorStreaming(Streaming_Mosaic_Id));
+            test::AssertBalances(context.cache(), Replicator_Key, { model::Mosaic{ Currency_Mosaic_Id, Expected_Replicator_Balance } });
         }
     }
 
     TEST(TEST_CLASS, ReplicatorOffboarding_Commit) {
-        // Arrange:
-        ReplicatorValues values;
-
         // Assert:
-        RunTest(NotifyMode::Commit, values, Current_Height);
+        RunTest(NotifyMode::Commit);
+    }
+
+    TEST(TEST_CLASS, DriveClosure_Rollback) {
+        // Assert
+		EXPECT_THROW(RunTest(NotifyMode::Rollback), catapult_runtime_error);
     }
 }}
