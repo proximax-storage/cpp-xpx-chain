c18a33556176b3fa189d3249d68ad98f157b8a18
cyrillicw <cyrillicw@gmail.com>
2021-09-24 20:25:41 +0300

    Tests OK

--- extensions/fastfinality/tests/WeightedVotingSyncActionTests.cpp
+++ extensions/fastfinality/tests/WeightedVotingSyncActionTests.cpp
@@ -18,6 +18,7 @@
 *** along with Catapult. If not, see <http://www.gnu.org/licenses/>.
 **/
 
+#include <utility>
 #include <vector>
 #include <fastfinality/src/WeightedVotingChainPackets.h>
 #include <catapult/model/Elements.h>
@@ -28,6 +29,7 @@
 #include <fastfinality/src/WeightedVotingFsm.h>
 #include <tests/test/core/mocks/MockCommitteeManager.h>
 #include <fastfinality/src/WeightedVotingActions.h>
+#include <map>
 
 namespace catapult {
 
@@ -35,122 +37,222 @@ namespace catapult {
 
 #define TEST_CLASS WeightedVotingSyncActionTests
 
+		namespace {
 
-		constexpr uint8_t Committee_Size = 21u;
-		constexpr uint64_t ConstantImportance = 1000000u;
-		constexpr uint64_t MaxHeight = 64u;
+			constexpr uint8_t Committee_Size = 21u;
+			constexpr double CommitteeEndSyncApproval = 0.45;
+			constexpr uint64_t CommitteeBaseTotalImportance = 100;
+			constexpr double CommitteeNotRunningContribution = 0.5;
 
-		//		void RunCreateDefaultCheckLocalChainActionTest(int correct_action,
-		//			    std::shared_ptr<catapult::fastfinality::RemoteNodeStateRetriever> retriever,
-		//				std::shared_ptr<config::BlockchainConfigurationHolder> pConfigHolder,
-		//				std::shared_ptr<model::BlockElementSupplier> lastBlockElementSupplier,
-		//				std::shared_ptr<std::function<uint64_t (const Key&)>> importanceGetter) {
-		//			std::shared_ptr<thread::IoThreadPool> pPool = test::CreateStartedIoThreadPool();
-		//			auto fsmShared = std::make_shared<fastfinality::WeightedVotingFsm>(pPool);
-		//			// auto fsmWeak = std::weak_ptr<fastfinality::WeightedVotingFsm>(fsmShared);
-		//			auto committeeManager = std::make_shared<mocks::MockCommitteeManager>();
-		//			auto counter = std::make_shared<int>(0);
-		//			auto & actions = fsmShared->actions();
-		//			actions.CheckLocalChain = [correct_action, fsmShared,
-		//									   retriever, pConfigHolder, lastBlockElementSupplier,
-		//									   importanceGetter, committeeManager] () {
-		//				if (z == 0) {
-		//					z += 1;
-		//					auto weakFsm = std::weak_ptr<fastfinality::WeightedVotingFsm>(fsmShared);
-		//					fastfinality::CreateDefaultCheckLocalChainAction(
-		//							weakFsm, *retriever, pConfigHolder,
-		//							*lastBlockElementSupplier, *importanceGetter, *committeeManager)();
-		//				}	else {
-		//					ASSERT_EQ(correct_action, 0);
-		//				}
-		//			};
-		//			actions.ResetLocalChain = [correct_action] {
-		//				ASSERT_EQ(correct_action, 1);
-		//			};
-		//			actions.DownloadBlocks = [correct_action] {
-		//				ASSERT_EQ(correct_action, 2);
-		//			};
-		//			actions.DetectStage = [correct_action] {
-		//				ASSERT_EQ(correct_action, 3);
-		//			};
-		//			fsmShared->start();
-		//		}
+			constexpr uint8_t CheckLocalChainActionCode = 0;
+			constexpr uint8_t ResetLocalChainActionCode = 1;
+			constexpr uint8_t DownloadBlocksActionCode = 2;
+			constexpr uint8_t DetectStageActionCode = 3;
 
-		uint64_t ConstantImportanceGenerator(const Key& key) {
-			return ConstantImportance;
-		}
+			class WeightedVotingSyncActionTestRunner
+				: public std::enable_shared_from_this<WeightedVotingSyncActionTestRunner> {
+			public:
+				WeightedVotingSyncActionTestRunner(
+						std::vector<fastfinality::RemoteNodeState> states,
+						std::shared_ptr<config::BlockchainConfigurationHolder> configHolder,
+						std::shared_ptr<model::BlockElement> lastBlockElement,
+						std::map<Key, uint64_t> importance,
+						uint8_t correctAction)
+					: states(std::move(states))
+					, configHolder(configHolder)
+					, lastBlockElement(lastBlockElement)
+					, importance(importance)
+					, correctAction(correctAction)
+					, counter(0) {
+					pool = test::CreateStartedIoThreadPool();
+					fsm = std::make_shared<fastfinality::WeightedVotingFsm>(pool);
+				};
 
-		auto GenerateStates() {
-			std::vector<fastfinality::RemoteNodeState>states;
-			for (int i = 0; i < Committee_Size; i++) {
-				fastfinality::RemoteNodeState r;
-				r.Height = Height(MaxHeight);
-				r.BlockHash = {{1u}};
-				r.NodeKey = {{(unsigned char ) i}};
-				r.NodeWorkState = fastfinality::NodeWorkState::Synchronizing;
-				r.HarvesterKeys = {r.NodeKey};
-				states.push_back(r);
-			}
-			return states;
-		}
-
-		auto GenerateBlockElement() {
-			model::Block b;
-			b.Height = Height(MaxHeight);
-			auto element = new model::BlockElement(b);
-			element->EntityHash = {{1u}};
-			return std::shared_ptr<model::BlockElement>(element);
-		}
-
-		auto CreateConfigHolder() {
-			auto config = model::NetworkConfiguration::Uninitialized();
-			config.CommitteeEndSyncApproval = 0.45;
-			config.CommitteeBaseTotalImportance = 100;
-			config.CommitteeNotRunningContribution = 0.5;
-			return std::make_shared<config::MockBlockchainConfigurationHolder>(config);
-		}
-
-		auto Retriever() {
-			return thread::make_ready_future(GenerateStates());
-		}
-
-		auto Supplier() {
-			return GenerateBlockElement();
-		}
-
-		void RunTemp() {
-			std::shared_ptr<thread::IoThreadPool> pPool = test::CreateStartedIoThreadPool();
-			auto fsmShared = std::make_shared<fastfinality::WeightedVotingFsm>(pPool);
-			auto configHolder = CreateConfigHolder();
-			auto committeeManager = std::make_shared<mocks::MockCommitteeManager>();
-			auto & actions = fsmShared->actions();
-			auto counter = std::make_shared<int>(0);
-			actions.CheckLocalChain = [counter, fsmShared, configHolder, committeeManager] {
-				if (*counter == 0) {
-					*counter += 1;
-					fastfinality::CreateDefaultCheckLocalChainAction(
-							fsmShared, Retriever, configHolder, Supplier, ConstantImportanceGenerator, *committeeManager)();
-				}	else {
-					// ASSERT_EQ(0, 0);
+				void Start() {
+					fsm->start();
+					pool->join();
 				}
-			};
-			actions.ResetLocalChain = [] {
-				// ASSERT_EQ(0, 1);
-			};
-			actions.DownloadBlocks = [] {
-				// ASSERT_EQ(0, 2);
-			};
-			actions.DetectStage = [] {
-				// ASSERT_EQ(0, 3);
-			};
-			fsmShared->start();
 
-			pPool->join();
+				void AssignActions() {
+					auto& actions = fsm->actions();
+					actions.CheckLocalChain = [t = shared_from_this()] {
+						if (t->counter == 0) {
+							t->counter = 1;
+							fastfinality::CreateDefaultCheckLocalChainAction(
+									t->fsm,
+									[t]() -> thread::future<std::vector<RemoteNodeState>> {
+										std::vector<RemoteNodeState> states(t->states.size());
+										for (int i = 0; i < t->states.size(); i++) {
+											states[i] = t->states[i];
+										}
+										return thread::make_ready_future(std::move(states));
+									},
+									t->configHolder,
+									[t] { return t->lastBlockElement; },
+									[t](const Key& key) -> uint64_t { return t->importance[key]; },
+									t->committeeManager)();
+						} else {
+							ASSERT_EQ(t->correctAction, CheckLocalChainActionCode);
+						}
+					};
+					actions.ResetLocalChain = [t = shared_from_this()] {
+						ASSERT_EQ(t->correctAction, ResetLocalChainActionCode);
+					};
+					actions.DownloadBlocks = [t = shared_from_this()] {
+						ASSERT_EQ(t->correctAction, DownloadBlocksActionCode);
+					};
+					actions.DetectStage = [t = shared_from_this()] {
+						ASSERT_EQ(t->correctAction, DetectStageActionCode);
+					};
+				}
+
+			private:
+				std::shared_ptr<WeightedVotingFsm> fsm;
+				std::vector<fastfinality::RemoteNodeState> states;
+				std::shared_ptr<config::BlockchainConfigurationHolder> configHolder;
+				std::shared_ptr<const model::BlockElement> lastBlockElement;
+				std::map<Key, uint64_t> importance;
+				mocks::MockCommitteeManager committeeManager;
+				std::shared_ptr<thread::IoThreadPool> pool;
+				uint8_t correctAction;
+				uint8_t counter;
+			};
+
+			auto GenerateStandardConfigHolder() {
+				auto config = model::NetworkConfiguration::Uninitialized();
+				config.CommitteeEndSyncApproval = CommitteeEndSyncApproval;
+				config.CommitteeBaseTotalImportance = CommitteeBaseTotalImportance;
+				config.CommitteeNotRunningContribution = CommitteeNotRunningContribution;
+				return std::make_shared<config::MockBlockchainConfigurationHolder>(config);
+			}
 		}
 
-		TEST(TEST_CLASS, SYNC) {
-			RunTemp();
+		namespace importance {
+
+			constexpr uint64_t StandardImportance = 1000000u;
+
+			auto GenerateEqualImportance(uint64_t value) {
+				std::map<Key, uint64_t> nodeImportance;
+				for (int i = 0; i < Committee_Size; i++) {
+					Key key = {{(unsigned char ) i}};
+					nodeImportance[key] = value;
+				}
+				return nodeImportance;
+			}
+		}
+
+		namespace states {
+
+			constexpr uint64_t MaxHeight = 64u;
+			constexpr uint64_t MinHeight = 16u;
+
+			constexpr Hash256 StandardHash = {{1u}};
+			constexpr Hash256 NonStandardHash = {{2u}};
+
+			auto GenerateBaseStates() {
+				std::vector<fastfinality::RemoteNodeState> states;
+				for (int i = 0; i < Committee_Size; i++) {
+					fastfinality::RemoteNodeState r;
+					r.NodeKey = {{(unsigned char ) i}};
+					r.HarvesterKeys = {r.NodeKey};
+					states.push_back(r);
+				}
+				return states;
+			}
+
+			void EqualStates(std::vector<fastfinality::RemoteNodeState> & states, NodeWorkState state,
+							 int from, int to) {
+				for (int i = from; i < to; i++) {
+					states[i].NodeWorkState = state;
+				}
+			}
+
+			void EqualHashes(std::vector<fastfinality::RemoteNodeState> & states, uint64_t height, Hash256 hash,
+							 int from, int to) {
+				for (int i = from; i < to; i++) {
+					states[i].Height = Height(height);
+					states[i].BlockHash = hash;
+				}
+			}
+		}
+
+		namespace blockelement {
+
+			auto GenerateBlockElement(uint64_t height, Hash256 hash) {
+				auto b = std::make_shared<model::Block>();
+				b->Height = Height(height);
+				auto element = std::make_shared<model::BlockElement>(b);
+				element->EntityHash = hash;
+				return element;
+			}
+
+		}
+
+		TEST(TEST_CLASS, GlobalReboot) {
+			auto nodeStates = states::GenerateBaseStates();
+			states::EqualHashes(nodeStates, states::MaxHeight, states::StandardHash, 0, Committee_Size);
+			states::EqualStates(nodeStates, NodeWorkState::Synchronizing, 0, Committee_Size);
+			auto nodeImportance = importance::GenerateEqualImportance(importance::StandardImportance);
+			auto config = GenerateStandardConfigHolder();
+			auto element = blockelement::GenerateBlockElement(states::MaxHeight, states::StandardHash);
+			auto runner = std::make_shared<WeightedVotingSyncActionTestRunner>(
+					nodeStates, config, element, nodeImportance, DetectStageActionCode);
+			runner->AssignActions();
+			runner->Start();
+		}
+
+		TEST(TEST_CLASS, GlobalRebootLagBehind) {
+			auto nodeStates = states::GenerateBaseStates();
+			states::EqualHashes(nodeStates, states::MaxHeight, states::StandardHash, 0, Committee_Size);
+			states::EqualStates(nodeStates, NodeWorkState::Synchronizing, 0, Committee_Size);
+			auto nodeImportance = importance::GenerateEqualImportance(importance::StandardImportance);
+			auto config = GenerateStandardConfigHolder();
+			auto element = blockelement::GenerateBlockElement(states::MinHeight, states::NonStandardHash);
+			auto runner = std::make_shared<WeightedVotingSyncActionTestRunner>(
+					nodeStates, config, element, nodeImportance, DownloadBlocksActionCode);
+			runner->AssignActions();
+			runner->Start();
+		}
+
+		TEST(TEST_CLASS, GlobalRebootNotEnoughApprovalRating) {
+			auto nodeStates = states::GenerateBaseStates();
+			states::EqualHashes(nodeStates, states::MaxHeight, states::StandardHash, 0, Committee_Size / 2);
+			states::EqualHashes(nodeStates, states::MinHeight, states::NonStandardHash, Committee_Size / 2, Committee_Size);
+			states::EqualStates(nodeStates, NodeWorkState::Synchronizing, 0, Committee_Size);
+			auto nodeImportance = importance::GenerateEqualImportance(importance::StandardImportance);
+			auto config = GenerateStandardConfigHolder();
+			auto element = blockelement::GenerateBlockElement(states::MaxHeight, states::NonStandardHash);
+			auto runner = std::make_shared<WeightedVotingSyncActionTestRunner>(
+					nodeStates, config, element, nodeImportance, CheckLocalChainActionCode);
+			runner->AssignActions();
+			runner->Start();
+		}
+
+		TEST(TEST_CLASS, GlobalRunningEnoughApprovalRating) {
+			auto nodeStates = states::GenerateBaseStates();
+			states::EqualHashes(nodeStates, states::MaxHeight, states::StandardHash, 0, Committee_Size / 2);
+			states::EqualHashes(nodeStates, states::MinHeight, states::NonStandardHash, Committee_Size / 2, Committee_Size);
+			states::EqualStates(nodeStates, NodeWorkState::Running, 0, Committee_Size);
+			auto nodeImportance = importance::GenerateEqualImportance(importance::StandardImportance);
+			auto config = GenerateStandardConfigHolder();
+			auto element = blockelement::GenerateBlockElement(states::MaxHeight, states::NonStandardHash);
+			auto runner = std::make_shared<WeightedVotingSyncActionTestRunner>(
+					nodeStates, config, element, nodeImportance, DetectStageActionCode);
+			runner->AssignActions();
+			runner->Start();
+		}
+
+		TEST(TEST_CLASS, InvalidChain) {
+			auto nodeStates = states::GenerateBaseStates();
+			states::EqualHashes(nodeStates, states::MinHeight, states::StandardHash, 0, Committee_Size);
+			states::EqualStates(nodeStates, NodeWorkState::Running, 0, Committee_Size);
+			auto nodeImportance = importance::GenerateEqualImportance(importance::StandardImportance);
+			auto config = GenerateStandardConfigHolder();
+			auto element = blockelement::GenerateBlockElement(states::MaxHeight, states::NonStandardHash);
+			auto runner = std::make_shared<WeightedVotingSyncActionTestRunner>(
+					nodeStates, config, element, nodeImportance, ResetLocalChainActionCode);
+			runner->AssignActions();
+			runner->Start();
 		}
 	}
-//
 }
