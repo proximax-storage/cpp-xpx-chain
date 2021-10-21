a25f0b6f191947f9f977c7c380a760e5c3c6aeed
kyrylo <cyrillicw@gmail.com>
2021-10-21 16:24:50 +0300

    Added Stream Validation Result

--- src/catapult/validators/ValidationResult.cpp
+++ src/catapult/validators/ValidationResult.cpp
@@ -61,6 +61,7 @@ namespace catapult { namespace validators {
 			#include "plugins/txes/property/src/validators/Results.h"
 			#include "plugins/txes/service/src/validators/Results.h"
 			#include "plugins/txes/storage/src/validators/Results.h"
+			#include "plugins/txes/streaming/src/validators/Results.h"
 			#include "plugins/txes/supercontract/src/validators/Results.h"
 			#include "plugins/txes/transfer/src/validators/Results.h"
 			#include "plugins/txes/upgrade/src/validators/Results.h"
