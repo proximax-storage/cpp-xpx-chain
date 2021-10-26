a69af49d5b82d7373bc955810a0544bebe35d244
oleg <otsybizov@gmail.com>
2021-10-26 12:32:01 +0300

    Disabled storage plugins.

--- tests/catapult/config/BlockchainConfigurationTests.cpp
+++ tests/catapult/config/BlockchainConfigurationTests.cpp
@@ -207,7 +207,7 @@ namespace catapult { namespace config {
 
 		void AssertDefaultSupportedEntityVersions(const SupportedEntityVersions& config) {
 			// Assert:
-			EXPECT_EQ(54u, config.size());
+			EXPECT_EQ(55u, config.size());
 		}
 	}
 
