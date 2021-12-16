d355ec43697a6d81517629c8efcb245715607774
warit <warit.hmk@gmail.com>
2021-12-16 11:56:24 +0800

    fix missing 'internal' directory

--- tools/prometheus-cpp/core/benchmarks/benchmark_helpers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstddef>
-#include <map>
-#include <string>
-
-std::string GenerateRandomString(std::size_t length);
-std::map<std::string, std::string> GenerateRandomLabels(
-    std::size_t number_of_labels);
