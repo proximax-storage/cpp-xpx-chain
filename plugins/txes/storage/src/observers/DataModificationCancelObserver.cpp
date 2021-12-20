9785c7f07bacf73123242d5fd50bd8a17a7cfd93
Yevhen <iorgachov1998@gmail.com>
2021-12-20 19:30:01 +0200

    delete wrong check if dataModification is first. Add refunds to `DataModificationCancel` observer.

--- plugins/txes/storage/src/observers/DataModificationCancelObserver.cpp
+++ plugins/txes/storage/src/observers/DataModificationCancelObserver.cpp
@@ -14,14 +14,49 @@ namespace catapult { namespace observers {
 
 	  	auto& driveCache = context.Cache.sub<cache::BcDriveCache>();
 		auto& driveEntry = driveCache.find(notification.DriveKey).get();
-		auto& activeDataModifications = driveEntry.activeDataModifications();
 
-		for (auto it = ++activeDataModifications.begin(); it != activeDataModifications.end(); ++it) {
-			if (it->Id == notification.DataModificationId) {
-				activeDataModifications.erase(it);
-				driveEntry.completedDataModifications().emplace_back(state::CompletedDataModification(*it, state::DataModificationState::Cancelled));
+		auto& activeDataModifications = driveEntry.activeDataModifications();
+		auto activeDataModificationIter = activeDataModifications.begin();
+
+		for (; activeDataModificationIter != activeDataModifications.end(); ++activeDataModificationIter) {
+			if (activeDataModificationIter->Id == notification.DataModificationId) {
+				activeDataModifications.erase(activeDataModificationIter);
+				driveEntry.completedDataModifications().emplace_back(state::CompletedDataModification{
+						*activeDataModificationIter,
+						state::DataModificationState::Cancelled
+				});
 				break;
 			}
 		}
+
+		const auto& streamingMosaicId = context.Config.Immutable.StreamingMosaicId;
+		auto& accountStateCache = context.Cache.sub<cache::AccountStateCache>();
+		auto driveOwnerIter = accountStateCache.find(driveEntry.owner());
+		auto& driveOwner = driveOwnerIter.get();
+
+		if (activeDataModificationIter->Id != activeDataModifications.front().Id) {
+			const auto refund = Amount(2 * activeDataModificationIter->ExpectedUploadSize * driveEntry.replicatorCount());
+			driveOwner.Balances.debit(streamingMosaicId, refund, context.Height);
+
+			return;
+		}
+
+		const auto downloadRefund = Amount(activeDataModificationIter->ExpectedUploadSize);
+		const auto uploadRefund = Amount(activeDataModificationIter->ExpectedUploadSize
+										 * (driveEntry.replicators().size() - 1) / driveEntry.replicators().size());
+
+		for (const auto& replicator: driveEntry.replicators()) {
+			auto replicatorIter = accountStateCache.find(replicator);
+			auto& rep = replicatorIter.get();
+
+			rep.Balances.debit(streamingMosaicId, downloadRefund, context.Height);
+			rep.Balances.debit(streamingMosaicId, uploadRefund, context.Height);
+		}
+
+		const auto clientUploadRefund = Amount(activeDataModificationIter->ExpectedUploadSize);
+		const auto streamingRefundAmount = Amount(2 * activeDataModificationIter->ExpectedUploadSize *
+											   (driveEntry.replicatorCount() - driveEntry.replicators().size()));
+		driveOwner.Balances.debit(streamingMosaicId, clientUploadRefund, context.Height);
+		driveOwner.Balances.debit(streamingMosaicId, streamingRefundAmount, context.Height);
 	})
 }}
