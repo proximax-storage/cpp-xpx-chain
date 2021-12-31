530f17a05496adce64c44df7096ce884ad9c9e99
ymol <molokanov9@gmail.com>
2021-12-31 15:10:04 +0200

    Update DriveClosureTransaction to V3.

--- plugins/txes/storage/src/observers/DriveClosureObserver.cpp
+++ plugins/txes/storage/src/observers/DriveClosureObserver.cpp
@@ -5,6 +5,7 @@
 **/
 
 #include "Observers.h"
+#include "src/utils/StorageUtils.h"
 
 namespace catapult { namespace observers {
 
@@ -16,10 +17,40 @@ namespace catapult { namespace observers {
 		auto driveIter = driveCache.find(notification.DriveKey);
 		auto& driveEntry = driveIter.get();
 
+	  	const auto& currencyMosaicId = context.Config.Immutable.CurrencyMosaicId;
+	  	const auto& streamingMosaicId = context.Config.Immutable.StreamingMosaicId;
+	  	auto& accountStateCache = context.Cache.sub<cache::AccountStateCache>();
+	  	auto driveStateIter = accountStateCache.find(notification.DriveKey);
+	  	auto& driveState = driveStateIter.get();
+	  	auto driveOwnerIter = accountStateCache.find(driveEntry.owner());
+	  	auto& driveOwnerState = driveOwnerIter.get();
+
+	  	// Making payments to replicators, if there is a pending data modification
+	  	auto& activeDataModifications = driveEntry.activeDataModifications();
+	  	if (!activeDataModifications.empty()) {
+			const auto& modificationSize = activeDataModifications.front().ExpectedUploadSize;
+		  	const auto& replicators = driveEntry.replicators();
+		  	const auto totalReplicatorAmount = Amount(
+				  	modificationSize +	// Download work
+				  	modificationSize * (replicators.size() - 1) / replicators.size());	// Upload work
+		  	for (const auto& replicatorKey : replicators) {
+			  	auto replicatorIter = accountStateCache.find(replicatorKey);
+			  	auto& replicatorState = replicatorIter.get();
+			  	driveState.Balances.debit(streamingMosaicId, totalReplicatorAmount, context.Height);
+			  	replicatorState.Balances.credit(currencyMosaicId, totalReplicatorAmount, context.Height);
+		  	}
+	  	}
+
+		// Returning the rest to the drive owner
+		const auto refundAmount = driveState.Balances.get(streamingMosaicId);
+	  	driveState.Balances.debit(streamingMosaicId, refundAmount, context.Height);
+	  	driveOwnerState.Balances.credit(currencyMosaicId, refundAmount, context.Height);
+
+		// Removing the drive from caches
 		auto& replicatorCache = context.Cache.sub<cache::ReplicatorCache>();
 		for (const auto& replicatorKey : driveEntry.replicators())
 			replicatorCache.find(replicatorKey).get().drives().erase(notification.DriveKey);
-		
+
 		driveCache.remove(notification.DriveKey);
 	}));
 }}
--- plugins/txes/storage/tests/observers/DriveClosureObserverTests.cpp
+++ plugins/txes/storage/tests/observers/DriveClosureObserverTests.cpp
@@ -20,14 +20,46 @@ namespace catapult { namespace observers {
         using Notification = model::DriveClosureNotification<1>;
 
         constexpr Height Current_Height(20);
+		const auto Owner_Key = test::GenerateRandomByteArray<Key>();
         constexpr auto Drive_Size = 100;
         constexpr auto Num_Replicators = 10;
+		constexpr auto Modification_Size = 10;
+		constexpr Amount Drive_Balance(200);
+		constexpr auto Currency_Mosaic_Id = MosaicId(1234);
+		constexpr auto Streaming_Mosaic_Id = MosaicId(4321);
+
+		constexpr Amount Expected_Replicator_Balance( Modification_Size * (2*Num_Replicators - 1) / Num_Replicators );
+		constexpr Amount Expected_Owner_Balance = Drive_Balance - Amount(Num_Replicators * Expected_Replicator_Balance.unwrap());
+
+
+		auto CreateConfig() {
+			test::MutableBlockchainConfiguration config;
+			config.Immutable.CurrencyMosaicId = Currency_Mosaic_Id;
+			config.Immutable.StreamingMosaicId = Streaming_Mosaic_Id;
+			return config.ToConst();
+		}
+
+		void AddAccountState(
+				cache::AccountStateCacheDelta& accountStateCache,
+				const Key& publicKey,
+				const std::vector<model::Mosaic>& mosaics = {}){
+			accountStateCache.addAccount(publicKey, Current_Height);
+			auto accountStateIter = accountStateCache.find(publicKey);
+			auto& accountState = accountStateIter.get();
+			for (auto& mosaic : mosaics)
+				accountState.Balances.credit(mosaic.MosaicId, mosaic.Amount);
+		}
 
         state::BcDriveEntry CreateInitialBcDriveEntry(const Key& driveKey, const utils::KeySet& replicatorKeys){
             state::BcDriveEntry entry(driveKey);
+			entry.setOwner(Owner_Key);
             entry.setSize(Drive_Size);
             entry.setReplicatorCount(Num_Replicators);
 			entry.replicators() = replicatorKeys;
+			entry.activeDataModifications().emplace_back(state::ActiveDataModification {
+					test::GenerateRandomByteArray<Hash256>(), test::GenerateRandomByteArray<Key>(),
+					test::GenerateRandomByteArray<Hash256>(), Modification_Size
+			});
 
             return entry;
         }
@@ -61,16 +93,21 @@ namespace catapult { namespace observers {
 
         void RunTest(NotifyMode mode, const CacheValues& values, const Height& currentHeight) {
             // Arrange:
-            ObserverTestContext context(mode, Current_Height);
+            ObserverTestContext context(mode, Current_Height, CreateConfig());
             Notification notification(values.InitialBcDriveEntry.key(), test::GenerateRandomByteArray<Key>());
             auto pObserver = CreateDriveClosureObserver();
             auto& bcDriveCache = context.cache().sub<cache::BcDriveCache>();
         	auto& replicatorCache = context.cache().sub<cache::ReplicatorCache>();
+			auto& accountStateCache = context.cache().sub<cache::AccountStateCache>();
 
             // Populate cache.
             bcDriveCache.insert(values.InitialBcDriveEntry);
-            for (const auto& entry : values.InitialReplicatorEntries)
-        		replicatorCache.insert(entry);
+			AddAccountState(accountStateCache, values.InitialBcDriveEntry.key(), {{Streaming_Mosaic_Id, Drive_Balance}});
+			AddAccountState(accountStateCache, values.InitialBcDriveEntry.owner());
+            for (const auto& entry : values.InitialReplicatorEntries) {
+				replicatorCache.insert(entry);
+				AddAccountState(accountStateCache, entry.key());
+			}
 
             // Act:
             test::ObserveNotification(*pObserver, notification, context);
@@ -82,7 +119,9 @@ namespace catapult { namespace observers {
 				auto replicatorIter = replicatorCache.find(entry.key());
 				const auto &actualEntry = replicatorIter.get();
 				test::AssertEqualReplicatorData(entry, actualEntry);
+				EXPECT_EQ(accountStateCache.find(entry.key()).get().Balances.get(Currency_Mosaic_Id), Expected_Replicator_Balance);
 			}
+			EXPECT_EQ(accountStateCache.find(Owner_Key).get().Balances.get(Currency_Mosaic_Id), Expected_Owner_Balance);
         }
     }
 
