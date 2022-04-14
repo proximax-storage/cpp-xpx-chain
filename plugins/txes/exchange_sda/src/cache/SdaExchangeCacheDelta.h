d79e0ed63fe2185ce9ba615d262b1f803c8de8a5
wernling-proximax <wernling.tan@proximax.io>
2022-04-14 14:56:47 +0800

    Remove unused m_pConfigHolder

--- plugins/txes/exchange_sda/src/cache/SdaExchangeCacheDelta.h
+++ plugins/txes/exchange_sda/src/cache/SdaExchangeCacheDelta.h
@@ -15,63 +15,62 @@
 
 namespace catapult { namespace cache {
 
-	/// Mixins used by the SDA-SDA exchange cache delta.
-	struct SdaExchangeCacheDeltaMixins : public PatriciaTreeCacheMixins<SdaExchangeCacheTypes::PrimaryTypes::BaseSetDeltaType, SdaExchangeCacheDescriptor> {
-		using Pruning = HeightBasedPruningMixin<
-			SdaExchangeCacheTypes::PrimaryTypes::BaseSetDeltaType,
-			SdaExchangeCacheTypes::HeightGroupingTypes::BaseSetDeltaType>;
-	};
+    /// Mixins used by the SDA-SDA exchange cache delta.
+    struct SdaExchangeCacheDeltaMixins : public PatriciaTreeCacheMixins<SdaExchangeCacheTypes::PrimaryTypes::BaseSetDeltaType, SdaExchangeCacheDescriptor> {
+        using Pruning = HeightBasedPruningMixin<
+            SdaExchangeCacheTypes::PrimaryTypes::BaseSetDeltaType,
+            SdaExchangeCacheTypes::HeightGroupingTypes::BaseSetDeltaType>;
+    };
 
-	/// Basic delta on top of the SDA-SDA exchange cache.
-	class BasicSdaExchangeCacheDelta
-			: public utils::MoveOnly
-			, public SdaExchangeCacheDeltaMixins::Size
-			, public SdaExchangeCacheDeltaMixins::Contains
-			, public SdaExchangeCacheDeltaMixins::ConstAccessor
-			, public SdaExchangeCacheDeltaMixins::MutableAccessor
-			, public SdaExchangeCacheDeltaMixins::PatriciaTreeDelta
-			, public SdaExchangeCacheDeltaMixins::BasicInsertRemove
-			, public SdaExchangeCacheDeltaMixins::Pruning
-			, public SdaExchangeCacheDeltaMixins::DeltaElements
-			, public SdaExchangeCacheDeltaMixins::ConfigBasedEnable<config::SdaExchangeConfiguration> {
-	public:
-		using ReadOnlyView = SdaExchangeCacheTypes::CacheReadOnlyType;
-		using SdaOfferOwners = SdaExchangeCacheTypes::HeightGroupingTypes::BaseSetDeltaType::ElementType::Identifiers;
+    /// Basic delta on top of the SDA-SDA exchange cache.
+    class BasicSdaExchangeCacheDelta
+            : public utils::MoveOnly
+            , public SdaExchangeCacheDeltaMixins::Size
+            , public SdaExchangeCacheDeltaMixins::Contains
+            , public SdaExchangeCacheDeltaMixins::ConstAccessor
+            , public SdaExchangeCacheDeltaMixins::MutableAccessor
+            , public SdaExchangeCacheDeltaMixins::PatriciaTreeDelta
+            , public SdaExchangeCacheDeltaMixins::BasicInsertRemove
+            , public SdaExchangeCacheDeltaMixins::Pruning
+            , public SdaExchangeCacheDeltaMixins::DeltaElements
+            , public SdaExchangeCacheDeltaMixins::ConfigBasedEnable<config::SdaExchangeConfiguration> {
+    public:
+        using ReadOnlyView = SdaExchangeCacheTypes::CacheReadOnlyType;
+        using SdaOfferOwners = SdaExchangeCacheTypes::HeightGroupingTypes::BaseSetDeltaType::ElementType::Identifiers;
 
-	public:
-		/// Creates a delta around \a sdaExchangeSets.
-		explicit BasicSdaExchangeCacheDelta(
-			const SdaExchangeCacheTypes::BaseSetDeltaPointers& sdaExchangeSets,
-			std::shared_ptr<config::BlockchainConfigurationHolder> pConfigHolder);
+    public:
+        /// Creates a delta around \a sdaExchangeSets.
+        explicit BasicSdaExchangeCacheDelta(
+            const SdaExchangeCacheTypes::BaseSetDeltaPointers& sdaExchangeSets,
+            std::shared_ptr<config::BlockchainConfigurationHolder> pConfigHolder);
 
-	public:
-		using SdaExchangeCacheDeltaMixins::ConstAccessor::find;
-		using SdaExchangeCacheDeltaMixins::MutableAccessor::find;
+    public:
+        using SdaExchangeCacheDeltaMixins::ConstAccessor::find;
+        using SdaExchangeCacheDeltaMixins::MutableAccessor::find;
 
-	public:
-		/// Adds offer expiry \a height of \a owner.
-		void addExpiryHeight(const SdaExchangeCacheDescriptor::KeyType& owner, const Height& height);
+    public:
+        /// Adds offer expiry \a height of \a owner.
+        void addExpiryHeight(const SdaExchangeCacheDescriptor::KeyType& owner, const Height& height);
 
-		/// Removes offer expiry \a height of \a owner.
-		void removeExpiryHeight(const SdaExchangeCacheDescriptor::KeyType& owner, const Height& height);
+        /// Removes offer expiry \a height of \a owner.
+        void removeExpiryHeight(const SdaExchangeCacheDescriptor::KeyType& owner, const Height& height);
 
-		/// Returns owners of offers expiring at \a height.
-		SdaOfferOwners expiringOfferOwners(Height height);
+        /// Returns owners of offers expiring at \a height.
+        SdaOfferOwners expiringOfferOwners(Height height);
 
-	private:
-		SdaExchangeCacheTypes::PrimaryTypes::BaseSetDeltaPointerType m_pSdaExchangeEntries;
-		SdaExchangeCacheTypes::HeightGroupingTypes::BaseSetDeltaPointerType m_pHeightGroupingDelta;
-		std::shared_ptr<config::BlockchainConfigurationHolder> m_pConfigHolder;
-	};
+    private:
+        SdaExchangeCacheTypes::PrimaryTypes::BaseSetDeltaPointerType m_pSdaExchangeEntries;
+        SdaExchangeCacheTypes::HeightGroupingTypes::BaseSetDeltaPointerType m_pHeightGroupingDelta;
+    };
 
-	/// Delta on top of the SDA-SDA exchange cache.
-	class SdaExchangeCacheDelta : public ReadOnlyViewSupplier<BasicSdaExchangeCacheDelta> {
-	public:
-		/// Creates a delta around \a sdaExchangeSets.
-		explicit SdaExchangeCacheDelta(
-			const SdaExchangeCacheTypes::BaseSetDeltaPointers& sdaExchangeSets,
-			std::shared_ptr<config::BlockchainConfigurationHolder> pConfigHolder)
-				: ReadOnlyViewSupplier(sdaExchangeSets, pConfigHolder)
-		{}
-	};
+    /// Delta on top of the SDA-SDA exchange cache.
+    class SdaExchangeCacheDelta : public ReadOnlyViewSupplier<BasicSdaExchangeCacheDelta> {
+    public:
+        /// Creates a delta around \a sdaExchangeSets.
+        explicit SdaExchangeCacheDelta(
+            const SdaExchangeCacheTypes::BaseSetDeltaPointers& sdaExchangeSets,
+            std::shared_ptr<config::BlockchainConfigurationHolder> pConfigHolder)
+                : ReadOnlyViewSupplier(sdaExchangeSets, pConfigHolder)
+        {}
+    };
 }}
