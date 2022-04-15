5d2190afa38ef5f9621f83197e5b9f0d391322ad
wernling-proximax <wernling.tan@proximax.io>
2022-04-15 21:49:57 +0800

    Change map.at() to map.find()

--- plugins/txes/exchange_sda/src/observers/RemoveSdaExchangeOfferObserver.cpp
+++ plugins/txes/exchange_sda/src/observers/RemoveSdaExchangeOfferObserver.cpp
@@ -12,17 +12,17 @@ namespace catapult { namespace observers {
 
     DEFINE_OBSERVER(RemoveSdaExchangeOfferV1, model::RemoveSdaOfferNotification<1>, [](const model::RemoveSdaOfferNotification<1>& notification, const ObserverContext& context) {
         auto& cache = context.Cache.sub<cache::SdaExchangeCache>();
-		auto iter = cache.find(notification.Owner);
-		auto& entry = iter.get();
-		SdaOfferExpiryUpdater sdaOfferExpiryUpdater(cache, entry);
+        auto iter = cache.find(notification.Owner);
+        auto& entry = iter.get();
+        SdaOfferExpiryUpdater sdaOfferExpiryUpdater(cache, entry);
 
         auto pSdaOffer = notification.SdaOffersPtr;
         for (uint8_t i = 0; i < notification.SdaOfferCount; ++i, ++pSdaOffer) {
             auto mosaicIdGive = context.Resolvers.resolve(pSdaOffer->MosaicIdGive);
             auto mosaicIdGet = context.Resolvers.resolve(pSdaOffer->MosaicIdGet);
 
-            Amount mosaicGiveAmount = entry.sdaOfferBalances().at(state::MosaicsPair{mosaicIdGive, mosaicIdGet}).CurrentMosaicGive;
-            Amount mosaicGetAmount = entry.sdaOfferBalances().at(state::MosaicsPair{mosaicIdGive, mosaicIdGet}).CurrentMosaicGet;
+            Amount mosaicGiveAmount = entry.sdaOfferBalances().find(state::MosaicsPair{mosaicIdGive, mosaicIdGet})->second.CurrentMosaicGive;
+            Amount mosaicGetAmount = entry.sdaOfferBalances().find(state::MosaicsPair{mosaicIdGive, mosaicIdGet})->second.CurrentMosaicGet;
             CreditAccount(entry.owner(), mosaicIdGive, mosaicGiveAmount, context);
 
             std::string reduced = reducedFraction(mosaicGiveAmount, mosaicGetAmount);
@@ -37,6 +37,6 @@ namespace catapult { namespace observers {
 
             entry.expireOffer(state::MosaicsPair{mosaicIdGive,mosaicIdGet}, context.Height);
             groupEntry.removeSdaOfferFromGroup(groupHash, notification.Owner);
-		}
+        }
     });
 }}
