aad569308b29a437655e6e2d420c528670cd5f03
nemesis_567 <>
2022-04-27 13:06:21 +0100

    Fix LockFundBlockObserver to properly unlock funds

--- plugins/txes/lock_fund/src/observers/LockFundBlockObserver.cpp
+++ plugins/txes/lock_fund/src/observers/LockFundBlockObserver.cpp
@@ -11,66 +11,69 @@
 
 namespace catapult { namespace observers {
 
-	/// Note: Consider removing records in batches instead
-	DEFINE_OBSERVER(LockFundBlock, model::BlockNotification<1>, ([](const auto& notification, const ObserverContext& context) {
-
-		const model::NetworkConfiguration& config = context.Config.Network;
-		auto& lockFundCache = context.Cache.sub<cache::LockFundCache>();
-		auto& accountStateCache = context.Cache.sub<cache::AccountStateCache>();
-		if (context.Mode == NotifyMode::Commit)
+	namespace {
+		void ProcessLockfundUpdate(const model::BlockNotification<1>& notification, const ObserverContext& context)
 		{
-			auto activeHeightRecord = lockFundCache.find(context.Height);
-			auto record = activeHeightRecord.tryGet();
-			if(record)
+			const model::NetworkConfiguration& config = context.Config.Network;
+			auto& lockFundCache = context.Cache.sub<cache::LockFundCache>();
+			auto& accountStateCache = context.Cache.sub<cache::AccountStateCache>();
+			if (context.Mode == NotifyMode::Commit)
 			{
-				// Unlock amounts
-				for(auto& irecord : record->LockFundRecords)
+				auto activeHeightRecord = lockFundCache.find(context.Height);
+				auto record = activeHeightRecord.tryGet();
+				if(record)
 				{
-					if(irecord.second.Active())
+					// Unlock amounts
+					for(auto& irecord : record->LockFundRecords)
+					{
+						if(irecord.second.Active())
+						{
+							auto &account = accountStateCache.find(irecord.first).get();
+							for(auto& mosaic : irecord.second.Get())
+							{
+								account.Balances.unlock(mosaic.first, mosaic.second, context.Height);
+							}
+						}
+					}
+					// Disable records
+					lockFundCache.disable(context.Height);
+				}
+				//Clear aged records
+				if (context.Height.unwrap() - config.MaxRollbackBlocks <= 0)
+					return;
+				auto clearHeight = context.Height-Height(config.MaxRollbackBlocks);
+				auto heightRecord = lockFundCache.find(clearHeight);
+				auto agedRecord = heightRecord.tryGet();
+				if(agedRecord)
+				{
+					// Clear records
+					lockFundCache.remove(clearHeight);
+				}
+			}
+			else
+			{
+				auto activeHeightRecord = lockFundCache.find(context.Height);
+				auto record = activeHeightRecord.tryGet();
+				if(record)
+				{
+					// Disable records
+					lockFundCache.recover(context.Height);
+					// Unlock amounts
+					for(auto& irecord : record->LockFundRecords)
 					{
 						auto account = accountStateCache.find(irecord.first).get();
-						for(auto& mosaic : irecord.second.Get())
+						if(irecord.second.Active()) //Redundant
 						{
-							account.Balances.unlock(mosaic.first, mosaic.second);
-						}
-					}
-				}
-				// Disable records
-				lockFundCache.disable(context.Height);
-			}
-			//Clear aged records
-			if (context.Height.unwrap() - config.MaxRollbackBlocks <= 0)
-				return;
-			auto clearHeight = context.Height-Height(config.MaxRollbackBlocks);
-			auto heightRecord = lockFundCache.find(clearHeight);
-			auto agedRecord = heightRecord.tryGet();
-			if(agedRecord)
-			{
-				// Clear records
-				lockFundCache.remove(clearHeight);
-			}
-		}
-		else
-		{
-			auto activeHeightRecord = lockFundCache.find(context.Height);
-			auto record = activeHeightRecord.tryGet();
-			if(record)
-			{
-				// Disable records
-				lockFundCache.recover(context.Height);
-				// Unlock amounts
-				for(auto& irecord : record->LockFundRecords)
-				{
-					auto account = accountStateCache.find(irecord.first).get();
-					if(irecord.second.Active()) //Redundant
-					{
-						for(auto& mosaic : irecord.second.Get())
-						{
-							account.Balances.lock(mosaic.first, mosaic.second);
+							for(auto& mosaic : irecord.second.Get())
+							{
+								account.Balances.lock(mosaic.first, mosaic.second);
+							}
 						}
 					}
 				}
 			}
 		}
-	}));
+	}
+	/// Note: Consider removing records in batches instead
+	DEFINE_OBSERVER(LockFundBlock, model::BlockNotification<1>, ProcessLockfundUpdate);
 }}
