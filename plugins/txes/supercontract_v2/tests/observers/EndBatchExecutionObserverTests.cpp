c6b74635275dcb69f0e665eafd6b4d2b6273f0fe
Yevhen <iorgachov1998@gmail.com>
2023-03-15 15:36:54 +0200

    add tests for ContractDestroy, EndBatchExecution, ManualCall observers

---
+++ plugins/txes/supercontract_v2/tests/observers/EndBatchExecutionObserverTests.cpp
@@ -0,0 +1,109 @@
+/**
+*** Copyright 2023 ProximaX Limited. All rights reserved.
+*** Use of this source code is governed by the Apache 2.0
+*** license that can be found in the LICENSE file.
+**/
+
+#include "src/observers/Observers.h"
+#include "tests/test/SuperContractTestUtils.h"
+#include "tests/test/plugins/ObserverTestUtils.h"
+#include "plugins/txes/storage/src/observers/StorageExternalManagementObserverImpl.h"
+
+namespace catapult { namespace observers {
+
+#define TEST_CLASS EndBatchExecutionObserverTests
+
+	DEFINE_COMMON_OBSERVER_TESTS(EndBatchExecution, {})
+
+	namespace {
+		using ObserverTestContext = test::ObserverTestContextT<test::SuperContractCacheFactory>;
+		using Notification = model::EndBatchExecutionNotification<1>;
+
+		const auto Current_Height = test::GenerateRandomValue<Height>();
+        const auto Super_Contract_Key = test::GenerateRandomByteArray<Key>();
+        const auto Batch_Id = 0;
+        const auto AutomaticExecutionsNextBlockToCheck = Height(0);
+        const auto Cosigners = std::vector<Key>{
+            test::GenerateRandomByteArray<Key>(),
+            test::GenerateRandomByteArray<Key>(),
+            test::GenerateRandomByteArray<Key>()
+        };
+
+        struct CacheValues {
+        public:
+            CacheValues() : InitialScEntry(Key()), ExpectedScEntry(Key()) {}
+
+        public:
+            state::SuperContractEntry InitialScEntry;
+            state::SuperContractEntry ExpectedScEntry;
+        };
+
+        void RunTest(NotifyMode mode, const CacheValues& values) {
+            // Arrange:
+            ObserverTestContext context(mode, Current_Height);
+            Notification notification(Super_Contract_Key, Batch_Id, AutomaticExecutionsNextBlockToCheck, Cosigners);
+            auto pObserver = CreateEndBatchExecutionObserver({});
+            auto& superContractCache = context.cache().sub<cache::SuperContractCache>();
+
+            // Populate cache.
+            superContractCache.insert(values.InitialScEntry);
+
+            // Act:
+            test::ObserveNotification(*pObserver, notification, context);
+
+            // Assert: check the cache
+            auto superContractCacheIter = superContractCache.find(values.InitialScEntry.key());
+            const auto actualScEntry = superContractCacheIter.tryGet();
+            EXPECT_TRUE(actualScEntry);
+            test::AssertEqualSuperContractData(values.ExpectedScEntry, *actualScEntry);
+        }
+	}
+
+	TEST(TEST_CLASS, Commit) {
+        // Arrange
+        CacheValues values;
+        values.InitialScEntry = state::SuperContractEntry(Super_Contract_Key);
+        values.InitialScEntry.batches().emplace(0, state::Batch{});
+        values.InitialScEntry.batches().emplace(1, state::Batch{});
+
+        for (uint64_t i = 0; i < Cosigners.size(); i++) {
+            values.InitialScEntry.executorsInfo()[Cosigners[i]] = {i};
+        }
+
+        values.ExpectedScEntry = values.InitialScEntry;
+        values.ExpectedScEntry.batches().emplace(values.InitialScEntry.nextBatchId(), state::Batch{});
+        values.ExpectedScEntry.automaticExecutionsInfo().AutomaticExecutionsNextBlockToCheck = AutomaticExecutionsNextBlockToCheck;
+
+		// Assert
+		RunTest(NotifyMode::Commit, values);
+	}
+
+//    TEST(TEST_CLASS, Commit_BadBatches) {
+//        // Arrange
+//        CacheValues values;
+//        values.InitialScEntry = state::SuperContractEntry(Super_Contract_Key);
+//        values.InitialScEntry.batches().emplace(0, state::Batch{});
+//
+//        for (uint64_t i = 0; i < Cosigners.size(); i++) {
+//            values.InitialScEntry.executorsInfo()[Cosigners[i]] = {i+values.InitialScEntry.nextBatchId()};
+//        }
+//
+//        values.ExpectedScEntry = values.InitialScEntry;
+//        values.ExpectedScEntry.batches().emplace(values.InitialScEntry.nextBatchId(), state::Batch{});
+//        values.ExpectedScEntry.automaticExecutionsInfo().AutomaticExecutionsNextBlockToCheck = AutomaticExecutionsNextBlockToCheck;
+//        values.ExpectedScEntry.batches().erase(0);
+//
+//        // Assert
+//        RunTest(NotifyMode::Commit, values);
+//    }
+
+	TEST(TEST_CLASS, Rollback) {
+        // Arrange
+        CacheValues values;
+        values.InitialScEntry = state::SuperContractEntry(Super_Contract_Key);
+        values.ExpectedScEntry = values.InitialScEntry;
+
+		// Assert
+		EXPECT_THROW(RunTest(NotifyMode::Rollback, values), catapult_runtime_error);
+	}
+}}
