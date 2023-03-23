4fbeb4b67b38b84e4da900efe5793c9959073bf5
Yevhen <iorgachov1998@gmail.com>
2023-03-23 13:35:22 +0200

    refine and add test case for SuccessfulEndBatchExecutionObserver

--- plugins/txes/supercontract_v2/tests/observers/SuccessfulEndBatchObserverTests.cpp
+++ plugins/txes/supercontract_v2/tests/observers/SuccessfulEndBatchObserverTests.cpp
@@ -23,6 +23,7 @@ namespace catapult { namespace observers {
 
         const auto Current_Height = test::GenerateRandomValue<Height>();
         const auto Super_Contract_Key = test::GenerateRandomByteArray<Key>();
+        const auto Drive_Key = test::GenerateRandomByteArray<Key>();
         const auto BatchId = 0;
         const auto Storage_Hash = test::GenerateRandomByteArray<Hash256>();
         const auto Used_Size_Bytes = 20;
@@ -36,11 +37,17 @@ namespace catapult { namespace observers {
 
         struct CacheValues {
         public:
-            CacheValues() : InitialScEntry(Key()), ExpectedScEntry(Key()) {}
+            CacheValues() :
+                InitialScEntry(Key()),
+                ExpectedScEntry(Key()),
+                InitialBcDriveEntry(Key()),
+                ExpectedBcDriveEntry(Key()) {}
 
         public:
             state::SuperContractEntry InitialScEntry;
             state::SuperContractEntry ExpectedScEntry;
+            state::BcDriveEntry InitialBcDriveEntry;
+            state::BcDriveEntry ExpectedBcDriveEntry;
             bool UpdateStorageState;
         };
 
@@ -62,8 +69,10 @@ namespace catapult { namespace observers {
 
             auto pObserver = CreateSuccessfulEndBatchExecutionObserver(Storage_External_Manager);
             auto& superContractCache = context.cache().sub<cache::SuperContractCache>();
+            auto& bcDriveCache = context.cache().sub<cache::BcDriveCache>();
 
             // Populate cache.
+            bcDriveCache.insert(values.InitialBcDriveEntry);
             superContractCache.insert(values.InitialScEntry);
 
             // Act:
@@ -72,15 +81,32 @@ namespace catapult { namespace observers {
             // Assert: check the cache
             auto superContractCacheIter = superContractCache.find(values.InitialScEntry.key());
             const auto actualScEntry = superContractCacheIter.tryGet();
-            EXPECT_TRUE(actualScEntry);
+            ASSERT_TRUE(actualScEntry);
             test::AssertEqualSuperContractData(values.ExpectedScEntry, *actualScEntry);
+
+            auto bcDriveCacheIter = bcDriveCache.find(values.InitialBcDriveEntry.key());
+            const auto actualBcDriveEntry = bcDriveCacheIter.tryGet();
+            ASSERT_TRUE(actualBcDriveEntry);
+            EXPECT_EQ(values.ExpectedBcDriveEntry.rootHash(), actualBcDriveEntry->rootHash());
+            EXPECT_EQ(values.ExpectedBcDriveEntry.usedSizeBytes(), actualBcDriveEntry->usedSizeBytes());
+            EXPECT_EQ(values.ExpectedBcDriveEntry.metaFilesSizeBytes(), actualBcDriveEntry->metaFilesSizeBytes());
+            EXPECT_EQ(values.ExpectedBcDriveEntry.metaFilesSizeBytes(), actualBcDriveEntry->metaFilesSizeBytes());
+            EXPECT_EQ(values.ExpectedBcDriveEntry.ownerManagement(), actualBcDriveEntry->ownerManagement());
+            EXPECT_EQ(values.ExpectedBcDriveEntry.verification()->VerificationTrigger, actualBcDriveEntry->verification()->VerificationTrigger);
+            EXPECT_EQ(values.ExpectedBcDriveEntry.verification()->Expiration, actualBcDriveEntry->verification()->Expiration);
+            EXPECT_EQ(values.ExpectedBcDriveEntry.verification()->Duration, actualBcDriveEntry->verification()->Duration);
+            EXPECT_EQ(values.ExpectedBcDriveEntry.verification()->Shards.size(), actualBcDriveEntry->verification()->Shards.size());
+            if (values.UpdateStorageState) {
+                EXPECT_NE(values.InitialBcDriveEntry.lastModificationId(), actualBcDriveEntry->lastModificationId());
+            }
         }
 	}
 
-	TEST(TEST_CLASS, Commit) {
+	TEST(TEST_CLASS, Commit_UpdateStorageState) {
         // Arrange
         CacheValues values;
         values.InitialScEntry = state::SuperContractEntry(Super_Contract_Key);
+        values.InitialScEntry.setDriveKey(Drive_Key);
         values.InitialScEntry.batches()[1] = {};
 
         values.ExpectedScEntry = values.InitialScEntry;
@@ -88,16 +114,49 @@ namespace catapult { namespace observers {
         batch.Success = true;
         batch.PoExVerificationInformation.fromBytes(Verification_Information);
 
-		values.UpdateStorageState = true;
+        values.InitialBcDriveEntry = state::BcDriveEntry(Drive_Key);
+        values.InitialBcDriveEntry.verification() = state::Verification();
+
+        values.ExpectedBcDriveEntry = values.InitialBcDriveEntry;
+        values.ExpectedBcDriveEntry.setRootHash(Storage_Hash);
+        values.ExpectedBcDriveEntry.setUsedSizeBytes(Used_Size_Bytes);
+        values.ExpectedBcDriveEntry.setMetaFilesSizeBytes(Meta_Files_Size_Bytes);
+        values.ExpectedBcDriveEntry.setOwnerManagement(state::OwnerManagement::PERMANENTLY_FORBIDDEN);
+        values.ExpectedBcDriveEntry.verification().reset();
+
+        values.UpdateStorageState = true;
 
 		// Assert
 		RunTest(NotifyMode::Commit, values);
 	}
 
+    TEST(TEST_CLASS, Commit_DoNotUpdateStorageState) {
+        // Arrange
+        CacheValues values;
+        values.InitialScEntry = state::SuperContractEntry(Super_Contract_Key);
+        values.InitialScEntry.setDriveKey(Drive_Key);
+        values.InitialScEntry.batches()[1] = {};
+
+        values.ExpectedScEntry = values.InitialScEntry;
+        auto& batch = values.ExpectedScEntry.batches()[1];
+        batch.Success = true;
+        batch.PoExVerificationInformation.fromBytes(Verification_Information);
+
+        values.InitialBcDriveEntry = state::BcDriveEntry(Drive_Key);
+        values.InitialBcDriveEntry.verification() = state::Verification();
+        values.ExpectedBcDriveEntry = values.InitialBcDriveEntry;
+
+        values.UpdateStorageState = false;
+
+        // Assert
+        RunTest(NotifyMode::Commit, values);
+    }
+
 	TEST(TEST_CLASS, Rollback) {
         // Arrange
         CacheValues values;
         values.InitialScEntry = state::SuperContractEntry(Super_Contract_Key);
+        values.InitialScEntry.setDriveKey(Drive_Key);
         values.ExpectedScEntry = values.InitialScEntry;
 
 		// Assert
