c9f90b28d12ba93e201b78df9ee44479e9bdcc27
kyrylo <cyrillicw@gmail.com>
2023-05-03 13:26:38 +0200

    Build fix

--- plugins/coresystem/src/validators/TransactionFeeValidator.cpp
+++ plugins/coresystem/src/validators/TransactionFeeValidator.cpp
@@ -28,6 +28,8 @@ namespace catapult { namespace validators {
 		if (notification.Fee > notification.MaxFee)
 			return Failure_Core_Invalid_Transaction_Fee;
 
+		CATAPULT_LOG(error) << "Fee validator";
+
 		constexpr auto Max_Raw_Block_Fee_Multiplier = static_cast<uint64_t>(std::numeric_limits<BlockFeeMultiplier::ValueType>::max());
 		return notification.MaxFee.unwrap() > Max_Raw_Block_Fee_Multiplier * notification.TransactionEntity.Size
 				? Failure_Core_Invalid_Transaction_Fee
